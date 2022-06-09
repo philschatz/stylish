718e23defb2865b960e23dda14e0133307ad70a3
philschatz <253202+philschatz@users.noreply.github.com>
2022-06-09 11:38:00 -0500

    :art: a little housekeeping

--- src/xml-example/runner.ts
+++ src/xml-example/runner.ts
@@ -4,6 +4,21 @@ import { DOMParser } from 'xmldom'
 import { useNamespaces } from 'xpath-ts'
 import { SourceMapGenerator } from 'source-map'
 
+type Pos = {
+    lineNumber: number,
+    columnNumber: number
+}
+type ComponentInfo = {
+    subselector: string
+    pos: Pos
+}
+type CSSRule = {
+    selector: string
+    props: Map<string, [Pos, string]> // e.g. "color" -> "blue"
+    componentName: string
+    pos: Pos
+}
+
 class ParseError extends Error { }
 
 function assertValue<T>(v: T | null | undefined) {
@@ -11,23 +26,9 @@ function assertValue<T>(v: T | null | undefined) {
     throw new Error('BUG: Expected a value but did not get anything')
 }
 
-function cssPropsToString(props: any) {
-    const lines: string[] = []
-    lines.push('{')
-    for (const [key, value] of Object.entries(props)) {
-        let v = value as string
-        if (Array.isArray(value)) {
-            v = value[1]
-        }
-        lines.push(`${key}: ${v};`)
-    }
-    lines.push('}')
-    return lines
-}
-
 function parseXML(fileContent: string) {
     const locator = { lineNumber: 0, columnNumber: 0 }
-    const cb = (msg: string) => {
+    const cb = () => {
         const pos = {
             line: locator.lineNumber - 1,
             character: locator.columnNumber - 1
@@ -45,13 +46,9 @@ function parseXML(fileContent: string) {
     return p.parseFromString(fileContent)
 }
 
-export const select = useNamespaces({ })
-export const selectOne = (sel: string, doc: Node): Element => {
-  const ret = select(sel, doc) as Node[]
-  return ret[0] as Element
-}
+const select = useNamespaces({ })
 
-function attrsToPairs(el: Element, exception?: string) {
+function attrsToProps(el: Element, exception?: string) {
     const pairs = new Map<string, [Pos, string]>()
     for (const attr of Array.from(el.attributes)) {
         if (attr.name === exception) continue
@@ -61,11 +58,11 @@ function attrsToPairs(el: Element, exception?: string) {
 }
 
 
-
+// HACK: The actual style framework has one more level of indirection but this is a prototype
 function camelToDash(str: string){
     return str.replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();})
 }
-
+// HACK: Instead of actually evaluating the template, wrap these templated strings in quotes so CSS does not complain
 function hackTemplate(str: string) {
     if (str.startsWith('{')) return `"${str}"`
     return str
@@ -75,23 +72,14 @@ function hackTemplate(str: string) {
 
 
 
+const designDOM = parseXML(readFileSync(join(__dirname, 'design.xml'), 'utf-8'))
 const bookStr = readFileSync(join(__dirname, 'chemistry.xml'), 'utf-8')
 const bookDOM = parseXML(bookStr)
-const designDOM = parseXML(readFileSync(join(__dirname, 'design.xml'), 'utf-8'))
+
+const instances = select('/book-root/*', bookDOM) as Element[]
 
 const componentDOMClasses = select('//component', designDOM) as Element[]
-
-type Pos = {
-    lineNumber: number,
-    columnNumber: number
-}
-type ComponentInfo = {
-    subselector: string
-    pos: Pos
-}
-
 const componentClasses = new Map<string, ComponentInfo>()
-
 for (const el of componentDOMClasses) {
     componentClasses.set(assertValue(el.getAttribute('name')), {
         subselector: assertValue(el.getAttribute('subselector')),
@@ -99,30 +87,25 @@ for (const el of componentDOMClasses) {
     })
 }
 
-const instances = select('/book-root/*', bookDOM) as Element[]
 
-type CSSRule = {
-    pos: Pos
-    selector: string
-    pairs: Map<string, [Pos, string]>
-}
 
 const cssRules: CSSRule[] = []
-
 for (const el of instances) {
     const attrs = Array.from(el.attributes)
     const selector = assertValue(el.getAttribute('selector'))
 
     if (attrs.length > 1) {
-        cssRules.push({ pos: el as unknown as Pos, selector, pairs: attrsToPairs(el, 'selector') })
+        const sel = assertValue(attrs.find(a => a.name === 'selector'))
+        cssRules.push({ componentName: el.tagName, pos: sel as unknown as Pos, selector, props: attrsToProps(el, 'selector') })
     }
     for (const child of select('*', el) as Element[]) {
         const componentName = child.tagName
         const componentClass = assertValue(componentClasses.get(componentName))
         cssRules.push({
+            componentName,
             pos: child as unknown as Pos,
             selector: `${selector} ${componentClass.subselector}`,
-            pairs: attrsToPairs(child)
+            props: attrsToProps(child)
         })
     }
 }
@@ -130,39 +113,25 @@ for (const el of instances) {
 // Print the CSS out
 const g = new SourceMapGenerator()
 g.setSourceContent('chemistry.xml', bookStr)
-
 const theCSSFileLines: string[] = []
 
+function addLine(pos: Pos, text: string) {
+    g.addMapping({
+        source: 'chemistry.xml',
+        original: { line: pos.lineNumber, column: pos.columnNumber },
+        generated: { line: theCSSFileLines.length + 1, column: 1 }
+    })
+    for (const line of text.split('\n')) {
+        theCSSFileLines.push(line)
+    }
+}
+
 for (const r of cssRules) {
-    const pairs: any = {}
-    for (const [key, value] of Array.from(r.pairs.entries())) {
-        pairs[key] = value[1]
-        // position = { col: value[0].columnNumber, line: value[0].lineNumber }
-    }
-
     // Add a sourcemap entry for the CSS Selector
-    theCSSFileLines.push(`${r.selector} {`)
-    g.addMapping({
-        source: 'chemistry.xml',
-        original: { line: r.pos.lineNumber, column: r.pos.columnNumber },
-        generated: { line: theCSSFileLines.length, column: 1 }
-    })
-
-    for (const [key, value] of Array.from(r.pairs.entries())) {
-        theCSSFileLines.push(`${camelToDash(key)}: ${hackTemplate(value[1])};`)
-        g.addMapping({
-            source: 'chemistry.xml',
-            original: { line: value[0].lineNumber, column: value[0].columnNumber },
-            generated: { line: theCSSFileLines.length, column: 1 }
-        })
+    addLine(r.pos, `${r.selector} { hack: "${r.componentName}"; }`)
+    for (const [key, value] of Array.from(r.props.entries())) {
+        addLine(value[0], `${r.selector} { ${camelToDash(key)}: ${hackTemplate(value[1])}; }`)
     }
-
-    theCSSFileLines.push(`}`)
-    g.addMapping({
-        source: 'chemistry.xml',
-        original: { line: r.pos.lineNumber, column: r.pos.columnNumber },
-        generated: { line: theCSSFileLines.length, column: 1 }
-    })
 }
 
 theCSSFileLines.push('/*# sourceMappingURL=xml-example.css.map*/')
