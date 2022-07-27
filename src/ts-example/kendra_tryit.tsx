198636956c128b27e6c69a00cfadaf7ec81912e2
Kendra Swanson <kendraswan@gmail.com>
2022-07-27 16:36:21 -0400

    definition for Component.addDefined()

--- src/ts-example/kendra_tryit.tsx
+++ src/ts-example/kendra_tryit.tsx
@@ -3,22 +3,39 @@ enum Settings {
   OPTIONAL
 };
 
+type CSSProperty = {
+  name: string; // validation here for "string that is a valid CSS property name"?
+  value: string | Settings;
+};
+
+// Components
 abstract class Component {
   subSelector?: string;
-  // abstract parent: Component;
-  // abstract children: Component[];
-  protected defined: Map<string, string | Settings> = new Map();
+  protected defined: CSSProperty[] = [];
 
-  abstract addDefined(params: any): void
+  addDefined(params: any): void {
+    // parses params given as an object into CSSProperties and adds to `defined`
+    let param_str: string = JSON.stringify(params);
+    let keys: string[] | undefined = param_str.match(/"[\w]*":/g)?.map((val) => {
+        return val.replaceAll('\"', '').replaceAll('_', '-').replace(':', '');
+    });
+    let vals: string[] | undefined = param_str.match(/:"[\w\s]*"/g)?.map((val) => { // TODO: allow hex colors thru, other data
+        return val.replace(':', '').replaceAll('\"', '');
+    });
+    for (let i = 0; i < keys.length; i++) {
+      this.defined.push({name: keys[i], value: vals[i]})
+    }
+  } // TODO: undefined checks; replaceAll
 
   toCSS(parentSelector: string): string {
     // TODO: validation
-    return `${parentSelector}${this.subSelector} ${JSON.stringify(this.defined)}`
+    return `${parentSelector}${this.subSelector} {
+      ${this.defined[0].name}: ${this.defined[0].value};
+    }` // TODO: put in all attributes from defined
   }
 }
 
-// How to organize the params for a component? 
-// Can't define an interface within a component :/
+// Component example
 interface note1Params {
   background_color: string | Settings;
   border_bottom: string | Settings;
@@ -28,39 +45,38 @@ interface note1Params {
 class Note1 extends Component {
   public constructor() {
     super()
-    this.defined = new Map([
-      ['color', 'green'], 
-      ['font-family', 'monospace'],
-      ['background-color', 'Settings.REQUIRED'], // why does Settings.REQUIRED error?
-      ['border', 'Settings.OPTIONAL']
-    ]);
+    this.defined = [
+      { name: 'color', value: 'green' },
+      { name: 'font-family', value: 'monospace' },
+      { name: 'background-color', value: Settings.REQUIRED },
+      { name: 'border', value: Settings.OPTIONAL },
+    ]
     this.subSelector = ''
   }
 
   addDefined(params: note1Params): void {
-    this.defined.set('background-color', params.background_color);
+    super.addDefined(params);
   }
-  
-  // addDefinedOptional(border: string): void {
-  //   this.defined.set('border', border); // eg. 'solid 1px black'
-  // }
 }
 
 class NoteTitle extends Component {
   public constructor() {
     super()
-    this.defined = new Map([
-      ['color', 'red']
-    ])
+    this.defined = [
+      { name: 'color', value: 'red' }
+    ];
     this.subSelector = '[data-type="title"]';
   }
 
-  addDefinedRequired() {}
-  addDefinedOptional() {}
+  // no addDefined -- no params to define!
+  // TODO: enforce this so that addDefined can't be called?
 };
 
+
+// Shapes
 abstract class Shape {
   rootSelector: string;
+  componentTree: any; // TODO: type this
 
   constructor(rootSelector: string) {
     this.rootSelector = rootSelector;
@@ -68,18 +84,15 @@ abstract class Shape {
 
   buildComponentTree() {
     // TODO: weave components (which are fields on the shape)
-    // into a tree by setting parent & child attributes.
-    // Ideally some nice human-readable data would be passed in to do this
+    // into a tree by setting parent & child attributes, and creating the full subselector (can be missing root)
   }
 
   toCSS() {
     // Traverses component tree
     // weaves subselectors & creates selector rule + params
+    // calls buildComponentTree
   }
 }
-
-// const noteShape = new Shape(new Note1());
-// noteShape.noteTitle = new NoteTitle();
 
 class NoteShape extends Shape {
   // Component list as params of the shape, so that autocomplete will work
@@ -88,35 +101,35 @@ class NoteShape extends Shape {
 
   constructor(rootSelector: string) {
     super(rootSelector)
-    this.note1 = new Note1();
-    this.noteTitle = new NoteTitle();
-    // Here the component list is given in ___ notation
-    // and assembled into a tree
-    
-    this.buildComponentTree(
-      // TODO: decide notation
-      [this.note1, [
-        this.noteTitle
-      ]]
-    )
+    // all components on the shape are class properties + instantiated in the tree
+    // (lots of repetition...)
+    this.componentTree = {
+      'val': this.note1 = new Note1(),
+      'children': [
+        {'val': this.noteTitle = new NoteTitle()}
+      ]
+    };
   }
 };
 
 const noteShape = new NoteShape('[data-type="note"]');
+// add optional and required params
 noteShape.note1.addDefined({
-  background_color: 'blue',
-
-});
-// noteShape.note1.addDefinedRequired('blue'); // bad
-noteShape.note1.addDefinedRequired({
   background_color: 'blue', 
   border_bottom: 'solid 1px red', 
   line_height: '2'
 });
+// gives compile-time error if required params are not present!
+// noteShape.note1.addDefined({
+//   background_color: 'blue',
+// });
 
-// Another one
+// Another one, different selector
 const noteShape1 = new NoteShape('div[data-type="note"].different-class');
-noteShape.note1.addDefinedRequired({
+noteShape.note1.addDefined({
   background_color: 'green', 
   border_bottom: 'double 2px blue'
+  // optional param not needed!
 });
+
+noteShape.toCSS(); // generates CSS from component tree, errors if settings are unset
